73d684c67a859e5e399888b070c96dcd88e19216
aBucholc <a.bucholc@wspa.edu.pl>
2023-06-11 18:54:50 +0200

    repositories and services - MOV-15, MOV-16

--- src/main/java/com/example/moviemetrics/service/FilmServiceImpl.java
+++ src/main/java/com/example/moviemetrics/service/FilmServiceImpl.java
@@ -1,4 +1,56 @@
 package com.example.moviemetrics.service;
 
-public class FilmServiceImpl {
+import com.example.moviemetrics.model.Film;
+import com.example.moviemetrics.repository.FilmRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FilmServiceImpl implements FilmService {
+
+    private final FilmRepository filmRepository;
+
+
+    @Override
+    public List<Film> getAllFilms() {
+        return filmRepository.findAll();
+    }
+
+    @Override
+    public Optional<Film> getFilmById(Long id) {
+        return filmRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Film> createFilm(Film film) {
+        Film savedFilm = filmRepository.save(film);
+        return Optional.ofNullable(savedFilm);
+    }
+
+    @Override
+    public Optional<Film> updateFilm(Long id, Film film) {
+        return filmRepository.findById(id)
+                .map(existingFilm -> {
+                    existingFilm.setTitle(film.getTitle());
+                    existingFilm.setDirector(film.getDirector());
+                    existingFilm.setYear(film.getYear());
+                    return filmRepository.save(existingFilm);
+                });
+    }
+
+    @Override
+    public void deleteFilm(Long id) {
+        filmRepository.findById(id).ifPresent(film -> filmRepository.delete(film));
+    }
+
+    @Override
+    public void saveFilm(Film film) {
+        filmRepository.save(film);
+    }
+
 }
